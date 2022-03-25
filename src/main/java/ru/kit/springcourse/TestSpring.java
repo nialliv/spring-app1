eb16b1408da001e010b6d6e4f747fcd58bdf0734
nialliv <artemevnikita@yandex.ru>
2022-03-25 13:34:39 +0400

    checked what is scope in spring

--- src/main/java/ru/kit/springcourse/TestSpring.java
+++ src/main/java/ru/kit/springcourse/TestSpring.java
@@ -8,11 +8,17 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        boolean compare = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(compare);
+        System.out.println(firstMusicPlayer + " " + secondMusicPlayer);
+        firstMusicPlayer.setVolume(23);
+        System.out.println(firstMusicPlayer.getVolume() + " " + secondMusicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
