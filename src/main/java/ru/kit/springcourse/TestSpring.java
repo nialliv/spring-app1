571eb883c1df663666291a4e5c1c366271a6c1f8
Nialliv <artemjevnikita@ya.ru>
2022-05-18 11:51:25 +0400

    add singleton (scope="prototype")

--- src/main/java/ru/kit/springcourse/TestSpring.java
+++ src/main/java/ru/kit/springcourse/TestSpring.java
@@ -8,12 +8,19 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
 
+        System.out.println(comparison);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
         context.close();
     }
 }
